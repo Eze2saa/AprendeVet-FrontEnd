384e219a4c8f9f463abc2b33104a41a74397d6c3
Ezequiel Dos Santos <ezequiel_dossantos@edsa.com.ar>
2025-11-08 23:19:28 -0300

    Prueba para ver si esta tomando los datos de environment en vez de environment.prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,8 @@
 
 
 export const environment = {
-  apiBaseUrl: 'http://localhost:3000/api',
-  ocularVetUrl: 'http://localhost:56180'
+  // apiBaseUrl: 'http://localhost:3000/api',
+  // ocularVetUrl: 'http://localhost:56180'
+  apiBaseUrl: 'https://aprendevet-backend-ocularvet-oi4d.onrender.com/api',
+  ocularVetUrl: 'https://ocularvet.onrender.com'
 }
